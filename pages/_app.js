6722b8c6a0381f9fabb1867bcc520e4b6d1c46bd
Attenea <yolanda.angarita.30@gmail.com>
2022-07-21 21:57:31 -0500

    add global layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="h-screen w-full bg-gray-700 text-white flex flex-col  items-center">
+      <ul className="w-full flex max-w-screen-lg justify-between px-8">
+        <Link href="/">
+          <a>Inicio</a>
+        </Link>
+        <Link href="/about">
+          <a>Sobre Nosotros</a>
+        </Link>
+        <li>Iniciar Sesión</li>
+        <li>Crear Cuenta</li>
+      </ul>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,3 @@
-import Link from 'next/link';
-
 export default function Home() {
-  return (
-    <>
-      <Link href="/about">
-        <a>Navegar About</a>
-      </Link>
-    </>
-  );
+  return <h1>Hola</h1>;
 }
